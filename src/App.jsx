46db4a1636ce4b68c521a2139a189b0cb5c0a3af
Ivan Dobroznai <i.dobroznai@gmail.com>
2025-07-30 18:47:53 +0200

    refactor App.jsx: function App()

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+// Lessons
+import Lesson_02 from "./lessons/Lesson_02/Lesson_02";
+
+// Homeworks
 import Homework_02 from "./homework/Homework_02/Homework_02";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
+      {/* Lessons */}
+      {/* <Lesson_02 /> */}
+
+      {/* Homeworks */}
       <Homework_02 />
     </div>
   );
 }
+
 export default App;
