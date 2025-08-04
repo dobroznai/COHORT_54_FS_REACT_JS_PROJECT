b5f1b27f123aa14f174bd443474cb5b87590c86f
Ivan Dobroznai <i.dobroznai@gmail.com>
2025-08-04 11:41:01 +0200

    refactor vite.config,js: added default server port 3000

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  // сервер буде запускатись на http://localhost:3000
+  server: {
+    port: 3000,
+    strictPort: true, // виведе помилку, якщо порт 3000 зайнятий
+  },
+});
